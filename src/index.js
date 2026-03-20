91f319880f0f58613feb5f2e59c619c1753205e2
Juan Esteban Kalocai Scalezi <jeks2002gmail.com>
2026-03-20 20:06:11 -0300

    Creacion de pagina para obtener link de uso del bot y cambio a sricpt de indexhtml

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const tmi = require('tmi.js');
 
-// Configure el cliente para que escuche tu canal
+// Configure el cliente 
 const client = new tmi.Client({
     channels: [ 'shekssink' ] 
 });
 
-// Conecte a los servidores de Twitch
+// Conecte con los servidores de Twitch
 client.connect().catch(console.error);
 
 // Le indique qué hacer cuando alguien manda un mensaje
 client.on('message', (channel, tags, message, self) => {
-    // tags['display-name'] trae el nombre del usuario y message el texto
+    // tags['display-name'] para obetner el nombre del usuario y el mensaje
     console.log(`${tags['display-name']} dice: ${message}`);
 });
