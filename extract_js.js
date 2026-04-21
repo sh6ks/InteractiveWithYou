3cb43178944f4e382bfb7fdfe6f9408f10eb4a32
Juan Esteban Kalocai Scalezi <jeks2002gmail.com>
2026-04-21 01:02:09 -0300

    chore: remove analysis scripts from repo

--- extract_js.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('c:\\Users\\sheks\\Desktop\\InteractiveWithYou\\html\\overlay.html', 'utf8');
-const scriptStart = html.indexOf('<script type="module">') + '<script type="module">'.length;
-const scriptEnd = html.lastIndexOf('</script>');
-const js = html.substring(scriptStart, scriptEnd);
-fs.writeFileSync('c:\\Users\\sheks\\Desktop\\InteractiveWithYou\\overlay_script.js', js);
-console.log('Extracted overlay_script.js');
